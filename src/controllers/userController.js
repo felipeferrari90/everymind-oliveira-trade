89198b574365e18395199871090b0fa7ef9003ee
root <felipeferrari_90@hotmail.com>
2022-02-27 01:07:02 -0300

    fixed bugs login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,45 +2,51 @@ const sign = require('jwt-encode')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
+
+
+
+
 class UserController{
+    
     static async login(req,res){
         let inputUsermail = req.body.email
-        var salt = bcrypt.genSaltSync(10);
-        let hashPassword =  bcrypt.hashSync(req.body.password,salt)
-        console.log(hashPassword)
-        let data = await User.findAll({
+        let inputPassword = req.body.password
+        let data = await User.findOne({
          where: {
            email : inputUsermail,
            }
          }).catch(
-           (err) => {return "erro de autenticacao"+err}
+           (err) => {return "erro de autenticacao: "+err}
          )
-        if(bcrypt.compareSync(req.body.password, data[0].dataValues.password)){
-          console.log(data[0].dataValues.password)
+        if(await bcrypt.compare(inputPassword, data.password)){
           let jwt = sign({inputUsermail},'wordsecret')
           res.setHeader('Authorization', 'Bearer '+ jwt);
-          return { user: data[0], token: jwt, message: "login realizado com sucesso"}
+          return { user: data, token: jwt, message: "login realizado com sucesso"}
         }
-        return data
+        else{
+          return {error:  'senha invalida, tente novamente'}
+        }
     }
     static async signup(req, res){
         let inputUserName = req.body.name
         let inputUsermail = req.body.email
+        let inputPassword = req.body.password
         let inputCpf = req.body.cpf
         var salt = bcrypt.genSaltSync(10);
-        let hashPassword = bcrypt.hashSync(req.body.password,salt)
         const user = await User.create({
            name : inputUserName ,
            email : inputUsermail,
-           password : hashPassword,
+           password : bcrypt.hashSync(inputPassword,salt),
            cpf: inputCpf
         }).then(
            (user) => { return {user:user , message: "usuario cadastrado com sucesso"}}
         ).catch(
-           (err) => { return {error: err}}
+           (err) => { throw {error: err}}
         )
         return user
     }
+
+    //para fins de debug
     static async getAllUsers(req,res){
       const users = await User.findAll({
       })
